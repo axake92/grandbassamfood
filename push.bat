0eec9420504f0f8bfe4c4c150e40918e8be502d9
axake92 <ake.axel35@gmail.com>
2026-05-14 17:12:31 +0200

    test: verification formulaire devis Netlify Forms

--- push.bat
+++ push.bat
@@ -2,8 +2,6 @@
 SET PATH=C:\Program Files\nodejs;%PATH%
 cd /d C:\Users\axela\Desktop\grandbassamfood
 git add .
-git commit -m "feat: initial site grandbassamfood"
-git branch -M main
-git remote add origin https://github.com/axake92/grandbassamfood.git
-git push -u origin main
-echo === PUSH TERMINE ===
+git commit -m "test: verification formulaire devis Netlify Forms"
+git push
+echo === DEPLOY DECLENCHE ===
